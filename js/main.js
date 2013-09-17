548c92a614e7013e8981b2e5bbad906c17da9809
Andrew Babkin <ab@izh.com>
2013-09-17 07:28:08 +0400

    fixed

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@
     
     $(function(){
         console.log("initiated app");
+        
+        var model = kendo.observable({
+            name: "",
+            city: "",
+            phone: "",
+            commit: function(){
+                console.log("form commited", model.toJSON());
+                alert("Подтверждение формы");
+            }
+        });
+        kendo.bind($(".form"), model);
+        
     });
     
 }(jQuery))
