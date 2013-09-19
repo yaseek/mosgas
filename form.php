b6b741d621877a725ef52a87c6242eb56fcbf66b
Andrew Babkin <ab@izh.com>
2013-09-20 01:33:09 +0400

    at work

--- form.php
+++ form.php
@@ -2,6 +2,48 @@
 
 header("Content-Type: application/json");
 
-echo json_encode(array("result"=>"ok"));
+require_once 'core/class.phpmailer.php';
 
-?>
+function Text($str){
+    return iconv("utf-8", "windows-1251", $str);
+};
+
+if (count($_POST) > 0) {
+
+    $mail = new PHPMailer();
+    $mail->CharSet="windows-1251";
+    $mail->Encoding="quoted-printable";
+    $mail->Subject=Text("Новый заказ");
+    $mail->AddAddress("ab@izh.com", Text("Андрей Бабкин"));
+    $mail->AddAddress("e@bogatyreva.com", Text("Евгения Богатырёва"));
+    $mail->From="do-not-reply@".$_SERVER["SERVER_NAME"];
+    $mail->FromName=Text("Диспетчер МосГаз");
+    ob_start();
+    ?>
+        Имя: <?=$_POST["name"].PHP_EOL?>
+        Город: <?=$_POST["city"].PHP_EOL?>
+        Телефон: <?=$_POST["phone"].PHP_EOL?>
+
+С уважением,         
+Служба поддержки сайта "МОСГАЗИФИКАЦИЯ.РФ"
+http://<?=$_SERVER["SERVER_NAME"]?>       
+        
+    <?
+    $body = ob_get_contents();
+    ob_end_clean();
+    
+    //$mail->IsHTML(true);
+    $mail->Body=Text($body);
+    $mail->Send();
+
+    echo json_encode(array(
+            "result"=>"ok"
+        )
+    );
+} else {
+    echo json_encode(array(
+            "result"=>"ok",
+            "server"=>$_SERVER
+        )
+    );
+};
--- js/main.js
+++ js/main.js
@@ -8,20 +8,46 @@
             name: "",
             city: "",
             phone: "",
-            commit: function(){
-                console.log("form commited", model.toJSON());
-                $.ajax({
-                    url: "/form.php",
-                    type: "post",
-                    dataType: "json",
-                    data: model.toJSON(),
-                    success: function(out){
-                        alert("Подтверждение формы");
-                    },
-                    error: function(){
-                        alert("ошибки");
-                    }
-                });
+            messageSent: false,
+            commit: function(e){
+                console.log("form commited", e);
+                var form=$(e.target).parents(".form");
+                var errors=false;
+                var input;
+                var template=kendo.template($("#template-message").html());
+                if (errors=(window.model.name=="")){
+                    input = form.find(".input.person input");
+                    input.after(template({text:input.attr("validationMessage")}))
+                        .focus();
+                };
+                if ((!errors) && (errors = (window.model.city==""))){
+                    input = form.find(".input.globe input");
+                    input.after(template({text:input.attr("validationMessage")}))
+                        .focus()
+                };
+                if ((!errors) && (errors=(window.model.phone.length !== 10))){
+                    input = form.find(".input.phone input");
+                    input.after(template({text:input.attr("validationMessage")}))
+                        .focus()
+                };
+                if(!errors){
+                    $(".form .k-widget.k-tooltip.k-tooltip-validation").remove();
+                    var button = $(e.target);
+                    $.ajax({
+                        url: "/form.php",
+                        type: "post",
+                        dataType: "json",
+                        data: model.toJSON(),
+                        success: function(out){
+                            alert("Ваш заказ успешно отправлен!");
+                            window.model.set("messageSent", true);
+                            $(button).removeClass("orange");
+                        },
+                        error: function(){
+                            alert("ошибки");
+                        }
+                    });
+                };
             },
             inputFocusIn: function(e){
                 $(this).attr("data-reserve", $(this).attr("placeholder"));
@@ -29,6 +55,7 @@
             },
             inputFocusOut: function(e){
                 $(this).attr("placeholder", $(this).attr("data-reserve"));
+                $(".form .k-widget.k-tooltip.k-tooltip-validation").remove();
             }
         });
         var form = $(".form");
